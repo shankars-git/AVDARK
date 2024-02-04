fc92e217861e1e98539ac61f1e42259427513a7a
Siriwardanage Don Supun Madusanka <siriwardanage-don-supun.madusanka.5747@student.uu.se>
2024-02-04 19:43:51 +0100

    part1 fixed

--- lab_1/avdark-cache/avdark-cache.h
+++ lab_1/avdark-cache/avdark-cache.h
@@ -59,7 +59,7 @@ typedef struct {
          * HINT: You may need to change how the internal cache state
          * is stored
          */
-        avdc_cache_line_t *lines;
+        avdc_cache_line_t **lines;
 
         /**
          * Cache parameters. Use avdc_resize() update them.
