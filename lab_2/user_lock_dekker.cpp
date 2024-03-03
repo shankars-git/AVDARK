731188c5e1e31bab3217053c3d43ed081e560932
Siriwardanage Don Supun Madusanka <40136783+SDsupun@users.noreply.github.com>
2024-03-03 12:05:29 +0100

    solution for dekker's

--- lab_2/user_lock_dekker.cpp
+++ lab_2/user_lock_dekker.cpp
@@ -3,28 +3,23 @@
 user_lock_dekker::user_lock_dekker()
     : user_lock() {
     m_flag[0] = m_flag[1] = false;
-    m_turn = false;
+    m_turn = 0;
 }
 
 void user_lock_dekker::lock(int thread_id) {
-    // TODO: Implement the lock acquire part of the Dekker algorithm here
-    bool other_thread_id = !thread_id;
-    // false = 0, true = 1
-    bool turn = thread_id; 
-    m_flag[thread_id] = true;
-    
-    while(m_flag[other_thread_id]){
-        if(m_turn != turn ){
-            m_flag[thread_id] = false;
-            while (m_turn != turn){}
-            m_flag[thread_id] = true;
+    m_flag[thread_id].store(true);
+    // Need memory fence here
+    while (m_flag[(thread_id+1)%2].load() == true) {
+        if (m_turn.load() != thread_id) {
+            m_flag[thread_id].store(false);
+            while (m_turn.load() != thread_id) {}
+            m_flag[thread_id].store(true);
         }
     }
 }
 
 void user_lock_dekker::unlock(int thread_id) {
-
-    m_turn = !thread_id;
-    m_flag[thread_id] = false;
-    
+    m_turn.store((thread_id+1)%2);
+    m_flag[thread_id].store(false);
+    // Need memory fence here
 }
